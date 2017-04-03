493100b7e77b2042d4aafa523f8ad8710845e38f
NulledExceptions <mmarnell1@gmail.com>
2017-04-03 14:51:53 -0500

    adding model files

--- econtextauth/models/user/apikey.py
+++ econtextauth/models/user/apikey.py
@@ -8,11 +8,24 @@ secret
 status (ENABLED|DISABLED)
 name
 description
+secretID
 
 """
+import rethinkdb as r
 from remodel.models import Model
 
 
 class ApiKey(Model):
     belongs_to = ("User", )
+
+    @property
+    def json(self):
+        u"""
+        Returns this object as a JSON object
+        """
+        return self.fields.as_dict()
+
+    def __init__(self, name=None, secretID=None, secret=None, status=None, description=None, createdAt=None, *args, **kwargs ):
+        createdAt = createdAt or r.now()
+        super(ApiKey, self).__init__(name=name, password=secretID, email=secret, status=status, createdAt=createdAt, description=description)
 
