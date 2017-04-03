f5f1cae3d1653a9a9371cf882220826babc35150
NulledExceptions <mmarnell1@gmail.com>
2017-04-03 13:00:08 -0500

    write permissions test

--- econtextauth/engine/routes/users/user.py
+++ econtextauth/engine/routes/users/user.py
@@ -1,5 +1,10 @@
 import logging
 log = logging.getLogger('econtext')
+from pprint import pprint
+import remodel.utils
+import remodel.connection
+import rethinkdb as r
+
 
 
 class User:
@@ -15,14 +20,16 @@ class User:
         'users/user',
         'users/user/{userid}'
     ]
-    
+    remodel.connection.pool.configure(db="test")
     @staticmethod
     def get_route_constructor(*args, **kwargs):
         return User(*args)
     
     def __init__(self, econtext):
         self.econtext = econtext
-    
+
+
+
     def on_post(self, req, resp):
         """
         Create a new User.
@@ -41,8 +48,25 @@ class User:
         :param resp:
         :return:
         """
-        self.econtext.get('rethinkdb')
-        resp.body = "ok"
+
+        #this_user=User.create()
+        #self.econtext.get('rethinkdb')
+        db=self.econtext.get('rethinkdb')
+        body= req.context['body']
+        #print body['password']
+        #print body['email']
+        try:
+            db_json={
+                "email":body['email'],
+                "password":body['password']
+            }
+        except KeyError as e:
+            print "must include necessary values"
+            resp.body = "Could not create user, missing fields."
+            return False
+        test_instert=r.table('users').insert(db_json).run(db)
+
+        resp.body=test_instert
         return True
     
     def on_get(self, req, resp, userid):
@@ -54,7 +78,22 @@ class User:
         :param userid:
         :return:
         """
-        resp.body = "ok"
+        #assert userid is id
+        db=self.econtext.get('rethinkdb')
+        userid = userid or None
+        #user=User(userid)
+        #print 'user', user, pprint(vars(user))
+        # print(r.table('accounts')
+        #         .order_by(r.desc('id')).run(conn))
+        #return_body=r.table('accounts').get("35528c0a-4fd6-4b16-8b0e-7ac41a9ef398").run(conn)
+
+        test_body=r.table('users').get(userid).run(db)
+
+        print userid
+        print test_body
+        #pprint(vars(db))
+
+        resp.body=test_body
         return True
     
     def on_put(self, req, resp, userid):
@@ -71,9 +110,22 @@ class User:
         :param userid:
         :return:
         """
-        resp.body = "ok"
+        db=self.econtext.get('rethinkdb')
+        userid = userid or None
+
+
+        #check user exists
+        #check fields to populate
+        #update fields
+        #check_user=r.table('users').get(userid).replace(req.context['body']).run(conn)
+
+        #VALIDATE CONTEXT['BODY'] BEFORE UPDATING....
+        check_user = r.table('users').get(userid).update(req.context['body']).run(db)
+
+
+        resp.body = check_user
         return True
-    
+
     def on_delete(self, req, resp, userid):
         """
         Remove a user specified by the userid
@@ -85,5 +137,14 @@ class User:
         :param userid:
         :return:
         """
-        resp.body = "ok"
+
+        #This will not delete DB entry just change status to Deleted.
+        # check user exists
+        # check status?
+        # change status to deleted
+
+        db = self.econtext.get('rethinkdb')
+        userid = userid or None
+        check_user = r.table('users').get(userid).update({"status":"deleted"}).run(db)
+        resp.body = check_user
         return True
