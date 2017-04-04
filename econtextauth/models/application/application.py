8d75c61b8e1046d8e0e4aec22d9bf3c418767003
NulledExceptions <mmarnell1@gmail.com>
2017-04-04 10:10:34 -0500

    adding application model methods

--- econtextauth/models/application/application.py
+++ econtextauth/models/application/application.py
@@ -29,3 +29,29 @@ class Application(Model):
         createdAt = createdAt or r.now()
         modifiedAt = modifiedAt or r.now()
         super(Application, self).__init__(name=name, description=description, status=status, createdAt=createdAt, modifiedAt=modifiedAt, customData=customData)
+
+
+    @staticmethod
+    def create_new(name, description=None, status=None, createdAt=None, modifiedAt=None, customData=None,*args, **kwargs):
+        """
+        Create a new Apllication object
+
+
+        :param name:
+        :param description:
+        :param status:
+        :param createdAt:
+        :param modifiedAt:
+        :param customData:
+        :param args:
+        :param kwargs:
+        :return:
+        """
+
+        createdAt = createdAt or r.now()
+        modifiedAt = modifiedAt or r.now()
+
+        a = Application(name=name,customData=customData, description=description, status=status, createdAt=createdAt,
+                 modifiedAt=modifiedAt)
+        return a
+
