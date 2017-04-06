181ad61440953f600dbfd7c89cc8258fa5c6c295
NulledExceptions <mmarnell1@gmail.com>
2017-04-06 11:00:47 -0500

    updating search

--- econtextauth/engine/routes/users/search.py
+++ econtextauth/engine/routes/users/search.py
@@ -33,75 +33,13 @@ class Search:
         :return:
         """
 
-        """
-        regex? partial match? which fields to compare?
-        
-        """
-
-        # user_search=models.user.user.User.getAll()
-        # user_search=models.user.user.User.objects.query.filter((lambda user: user['email'].match('test2jkfkdjaklfjalksfj')) or (lambda user: user['name'].match('ma'))).run()
-        # user_search = models.user.user.User.objects.query.filter(
-        #     lambda user: (user['name'].match(search)) | (user['email'].match(search))).run()
-        #
-        # user_search2=models.user.user.User.objects.query.filter(
-        #     lambda user: (user['name'].match(search)) | (user['email'].match(search))).run()
+        user_search = models.user.user.User.objects.query.filter(
+            lambda user: (user['email'].match(search)) | (user['name'].match(search)) | (
+            user['id'].match(search))).run()
 
 
-        # user_search_nested.append(models.user.user.User.objects.query.filter(lambda user: (user['customData'].match(search))).map(lambda user:
-        #                                                                                             {'id': user['id'],
-        #                                                                                              'name': user[
-        #                                                                                                  'name'],
-        #                                                                                              'email': user[
-        #                                                                                                  'email']}).run())
-
-        # user_search_nested = []
-        #
-        # user_search_nested.append(models.user.user.User.objects.query.filter(lambda user : user['customData'].match(search)).run())
-        # for a in user_search_nested:
-        #     log.debug(a)
-        #
-        #
-        #
-        #
-        #
-        #
-        # # user_search0.append(models.user.user.User.objects.query.filter(
-        # #     lambda user: (user['email'].match(search))).run())
-        #
-        #
-        # #     user_search4=models.user.user.User.objects.query.map(lambda user:
-        # # { 'name': user['name'], 'id': user['id'] }).run()
-        #
-        # # log.debug(user_search)
-        # # log.debug(user_search2)
-        # log.debug(user_search_nested)
-        #
-
-
-        user_search0 = []
-        user_search0.append(models.user.user.User.objects.query.filter(
-            lambda user: (user['email'].match(search))).map(lambda user:
-                                                            {'id': user['id'], 'name': user['name'],
-                                                             'email': user['email']}).run())
-
-        user_search0.append(
-            models.user.user.User.objects.query.filter(lambda user: (user['email'].match(search))).map(lambda user:
-                                                                                                       {'id': user[
-                                                                                                           'id'],
-                                                                                                        'name': user[
-                                                                                                            'name'],
-                                                                                                        'email': user[
-                                                                                                            'email']}).run())
-        user_search0.append(
-            models.user.user.User.objects.query.filter(lambda user: (user['id'].match(search))).map(lambda user:
-                                                                                                    {'id': user['id'],
-                                                                                                     'name': user[
-                                                                                                         'name'],
-                                                                                                     'email': user[
-                                                                                                         'email']}).run())
-
-        for each_result in user_search0:
-            log.debug(each_result)
-        log.debug(user_search0)
-        resp.body = user_search0
+        id_search=models.user.user.User.objects.query.filter(lambda user: (user['id'].match(search))).run()
+        log.debug(user_search)
+        log.debug(id_search)
+        resp.body = user_search
         return True
