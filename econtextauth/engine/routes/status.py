0a6f48e99abf0c969e5b9e93f115fde9ab2a4f11
Jonathan Spalink <jspalink@info.com>
2017-08-10 16:03:26 -0500

    minor

--- econtextauth/engine/routes/status.py
+++ econtextauth/engine/routes/status.py
@@ -12,10 +12,14 @@ class Status:
         return Status()
 
     def on_get(self, req, resp):
-        rethink_connection = True
+        rethink_connection = False
         for conn in remodel.connection.get_conn().gen:
             if not conn.is_open():
-                rethink_connection = False
+                rethink_connection = True
+                break
+        
+        if not rethink_connection:
+            raise Exception("Could not connect to RethinkDB database")
         
         resp.body = {
             "rethinkdb": rethink_connection
